bacececbeb3f4bee11e44343826f3246a2f50d7e
yamakadoh <hello@yamakadoh.net>
2015-01-31 15:50:35 +0900

    UIToolbarを追加

--- SlideStatusAndToolBar/ViewController.swift
+++ SlideStatusAndToolBar/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDe
     let webView = UIWebView()
     var isShowStatusBar = false // ステータスバーの表示状態
     
+    var toolbar = UIToolbar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // -----------------------------
+        // UIWebView
+        // -----------------------------
         webView.delegate = self
         webView.frame = self.view.bounds
         self.view.addSubview(webView)
@@ -27,13 +32,32 @@ class ViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDe
         
         // ジェスチャー
         // タップ
-        var gestureTap = UITapGestureRecognizer(target: self, action: "onTap:")
+        var gestureTap = UITapGestureRecognizer(target: self, action: "onTapWebView:")
         gestureTap.delegate = self
         self.view.addGestureRecognizer(gestureTap)
         // ドラッグ
-        var gesturePan = UIPanGestureRecognizer(target: self, action: "onPan:")
+        var gesturePan = UIPanGestureRecognizer(target: self, action: "onPanWebView:")
         gesturePan.delegate = self
         self.view.addGestureRecognizer(gesturePan)
+        
+        // -----------------------------
+        // UIToolbar
+        // -----------------------------
+        toolbar.frame = CGRectMake(0, self.view.bounds.size.height - 44, self.view.bounds.size.width, 40.0)
+        toolbar.layer.position = CGPoint(x: self.view.bounds.size.width/2, y: self.view.bounds.size.height - 20)
+        //toolbar.barStyle = UIBarStyle.Default
+        //toolbar.tintColor = UIColor.whiteColor()
+        //toolbar.backgroundColor = UIColor.grayColor()
+        
+        let buttonMenu = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Action, target: self, action: "onTapToolbarMenu:")
+        buttonMenu.tag = 1
+        
+        let buttonSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action:nil)
+        
+        let buttonRefresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: self, action: "onTapToolbarRefresh:")
+        
+        toolbar.items = [buttonMenu, buttonSpace, buttonRefresh]
+        self.view.addSubview(toolbar)
     }
     
     override func didReceiveMemoryWarning() {
@@ -59,9 +83,9 @@ class ViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDe
         return UIStatusBarAnimation.Slide
     }
     
-    // タップイベントハンドラ
-    func onTap(sender: UITapGestureRecognizer) -> Void {
-        println("[onTap]called")
+    // UIWebViewタップイベントハンドラ
+    func onTapWebView(sender: UITapGestureRecognizer) -> Void {
+        println("[onTapWebView]called")
         
         // ステータスバーとツールバーの表示を切り替える
         isShowStatusBar = !isShowStatusBar
@@ -71,9 +95,9 @@ class ViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDe
         
     }
     
-    // ドラッグイベントハンドラ
-    func onPan(sender: UIPanGestureRecognizer) -> Void {
-        println("[onPan]called")
+    // UIWebViewドラッグイベントハンドラ
+    func onPanWebView(sender: UIPanGestureRecognizer) -> Void {
+        println("[onPanWebView]called")
         
         let point: CGPoint = sender.translationInView(self.view)
         println("Point = (\(point.x), \(point.y))")
@@ -112,6 +136,15 @@ class ViewController: UIViewController, UIWebViewDelegate, UIGestureRecognizerDe
     func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         // 本メソッドを実装しない(trueを返さない)と、自作したジェスチャイベントハンドラが呼ばれない
         return true
+    }
+    
+    // UIToolbarタップイベントハンドラ
+    func onTapToolbarMenu(sender: UIBarButtonItem) {
+        println("[onTapToolbarMenu]")
+    }
+    
+    func onTapToolbarRefresh(sender: UIBarButtonItem) {
+        println("[onTapToolbarRefresh]")
     }
 }
 
